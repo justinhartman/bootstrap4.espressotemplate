56cd4dbd5ce15ce9b323de513bf25da3ddde6985
Justin Hartman <justin@hartman.me>
2018-09-13 00:02:36 +0200

    Updated the generator settings and JS file.

--- _generator.js
+++ _generator.js
@@ -1,44 +1,55 @@
 var vendorScriptMap = {
-	bootstrap: 				'bootstrap-3.3.7.min.js',
-	html5shiv: 				'html5shiv-3.7.3.min.js',
-	respond:				'respond-1.4.2.min.js',
-	jquery:					'jquery-3.3.1.min.js',
-	modernizr:				'modernizr-3.6.0.min.js'
+	bootstrap: 				'bootstrap.min.js',
+	jquery:					'jquery-slim.min.js',
+    popper:                 'popper.min.js',
+    anchor:                 'anchor.min.js',
+    clipboard:              'clipboard.min.js',
+    holder:                 'holder.min.js'
+
 };
 
 generator.applyToOutputNode = function(outputFolderNode, inputFolderNode) {
-	
-	// Read config and set default values	
+
+	// Read config and set default values
 	var config = generator.config || {};
-	config.base = config.base || 'none';
-	config.polyfill = config.polyfill || 'modernizr';
+	config.base = config.base || 'starter';
 	config.serverExtras = !!config.serverExtras;
 	config.ieTags = (config.ieTags === undefined) ? true : !!config.ieTags;
 	config.ga = !!config.ga;
 	config.ga_siteId = config.ga ? config.ga_siteId || '' : undefined;
-	
-	// Add common files from H5BP
-	var boilerplateInputNode = inputFolderNode.folderForPath('html5-boilerplate_v6.1.0');
+
+	// Add common files from Bootstrap folder.
+	var boilerplateInputNode = inputFolderNode.folderForPath('bootstrap');
 	var boilerplateOutputNode = outputFolderNode.addFolderAtPath(boilerplateInputNode, './', OverwriteOnConflict);
-	
-    var excludePaths = ['css/', 'doc/', 'js/vendor/', 'index.html' ];
+
+    var excludePaths = [
+        'examples/',
+        'js/'
+    ];
 	if (!config.serverExtras) {
-		excludePaths = excludePaths.concat(['.htaccess', '404.html', 'humans.txt', 'robots.txt']);
+		excludePaths = excludePaths.concat([
+            '.htaccess',
+            '.gitignore',
+            '.gitattributes',
+            '.editorconfig',
+            'humans.txt',
+            'robots.txt'
+        ]);
 	}
-	
-	excludePaths.forEach(function(excludePath) { 
+
+	excludePaths.forEach(function(excludePath) {
 		var currentNode = null;
-		
+
 		if (excludePath.slice(-1) === '/') {
 			currentNode = boilerplateInputNode.folderForPath(excludePath);
 		}
 		else {
 			currentNode = boilerplateInputNode.fileForPath(excludePath);
 		}
-		
+
 		boilerplateOutputNode.excludeNode(currentNode);
 	});
-	
+
 	// Add vendor script libraries
 	function addVendorScript(vendorFileName) {
 		if (vendorFileName) {
@@ -46,26 +57,22 @@ generator.applyToOutputNode = function(outputFolderNode, inputFolderNode) {
 			outputFolderNode.addFileAtPath(vendorInputFile, 'js/vendor/' + vendorFileName, OverwriteOnConflict);
 		}
 	};
-	
+
+    addVendorScript(vendorScriptMap.bootstrap);
 	addVendorScript(vendorScriptMap.jquery);
-	addVendorScript(vendorScriptMap[config.polyfill]);
-	
-	if (config.base === 'responsive' || config.base === 'bootstrap') {
-		addVendorScript(vendorScriptMap.respond);
+
+	if (config.base === 'starter' || config.base === 'jumbotron') {
+		addVendorScript(vendorScriptMap.popper);
 	}
-		
-	if (config.base === 'bootstrap') {
-		addVendorScript(vendorScriptMap.bootstrap);
-	}
-	
+
 	// Add template styles
 	var styleInputFolderNode = inputFolderNode.folderForPath('templates/' + config.base);
-		
+
 	if (styleInputFolderNode !== null) {
 		for (var i = 0, styleChildNodes = styleInputFolderNode.childNodes; i < styleChildNodes.length; i++) {
 			var childNode = styleChildNodes[i];
 			var childNodePath = childNode.path.split('/').slice(2).join('/');
-			
+
 			if (childNode.type === FolderNode) {
 				outputFolderNode.addFolderAtPath(childNode, childNodePath);
 			}
@@ -74,25 +81,25 @@ generator.applyToOutputNode = function(outputFolderNode, inputFolderNode) {
 			}
 		}
 	}
-	
+
 	// Translate our configuration to the index page
 	var boilerplateIndexNode = outputFolderNode.fileForPath('index.html');
-	
+
 	if (boilerplateIndexNode !== null) {
-		boilerplateIndexNode.assignVariable(config.polyfill, 'true');
-		
+		// boilerplateIndexNode.assignVariable(config.polyfill, 'true');
+
 		if (config.ieTags) {
 			boilerplateIndexNode.assignVariable('ieTags', true);
 		}
-		
+
 		if (config.ga) {
 			boilerplateIndexNode.assignVariable('googleAnalytics', true);
 		}
-		
+
 		if (config.ga_siteId) {
 			boilerplateIndexNode.assignVariable('googleAnalyticsSiteID', config.ga_siteId);
 		}
 	}
-	
+
 	return true;
 };
